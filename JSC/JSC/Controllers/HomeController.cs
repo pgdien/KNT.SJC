bbcc5993a3b1ddbf051f080c29e064f0df0d6ea3
ThanhLa <lathanh1108@gmail.com>
2016-09-29 09:49:01 +0700

    Giao diện
    Html, CSS của trang

---
+++ JSC/JSC/Controllers/HomeController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult SvThietkewebsite()
+        {
+            return View();
+        }
+        public ActionResult SvThietkephanmem()
+        {
+            return View();
+        }
+        public ActionResult SvMarketingonline()
+        {
+            return View();
+        }
+        public ActionResult SvDichvukhac()
+        {
+            return View();
+        }
+        public ActionResult WsNhahang()
+        {
+            return View();
+        }
+        public ActionResult WsKhachsan()
+        {
+            return View();
+        }
+        public ActionResult WsSpa()
+        {
+            return View();
+        }
+        public ActionResult WsBanhang()
+        {
+            return View();
+        }
+        public ActionResult WsTintuc()
+        {
+            return View();
+        }
+        public ActionResult WsCongty()
+        {
+            return View();
+        }
+        public ActionResult WsDichvu()
+        {
+            return View();
+        }
+        public ActionResult DsNhahang()
+        {
+            return View();
+        }
+        public ActionResult DsKaraoke()
+        {
+            return View();
+        }
+        public ActionResult DsBanhang()
+        {
+            return View();
+        }
+        public ActionResult DsNhansu()
+        {
+            return View();
+        }
+        public ActionResult DsDoanvien()
+        {
+            return View();
+        }
+        public ActionResult DsInan()
+        {
+            return View();
+        }
+        public ActionResult MoQuantriweb()
+        {
+            return View();
+        }
+        public ActionResult MoQuantrifanpage()
+        {
+            return View();
+        }
+        public ActionResult MoGoogleadwords()
+        {
+            return View();
+        }
+        public ActionResult MoFacebookadwords()
+        {
+            return View();
+        }
+        public ActionResult MoSEOweb()
+        {
+            return View();
+        }
+        public ActionResult MoSEOfanpage()
+        {
+            return View();
+        }
+        public ActionResult OsPoster()
+        {
+            return View();
+        }
+        public ActionResult OsCard()
+        {
+            return View();
+        }
+        public ActionResult OsBangron()
+        {
+            return View();
+        }
+        public ActionResult OsDaotao()
+        {
+            return View();
+        }
+        public ActionResult OsCungcap()
+        {
+            return View();
+        }
+        public ActionResult OsTochuc()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ JSC/JSC/Scripts/Front/home.js
@@ -0,0 +1,50 @@
+﻿/*Những dự án tiêu biểu*/
+$(function () {
+    $("#next-to-nhungduantieubieu").click(function () {                                                             /*Click nút xuống nội dung chia sẻ*/
+        $('html, body').animate({
+            scrollTop: $('#duantieubieu').offset().top
+        }, 500);
+    });
+});
+
+$(".hover-project").hover(function () {
+    $(this).find(".box-info").slideToggle('fast');
+});
+
+
+
+/*JS cho another-service (Dịch vụ khác)*/
+$(function () {
+    var swap = true;
+    $('.another-close-button').click(function () {
+        if (swap === true) {
+            $('.another-close-button img').attr("src", "../images/Homepage/another-service-close-icon.png");         /*Thay đổi nút xem thêm -> đóng*/
+            $('.another-service .content .icon .another-other').slideDown("slow");                                   /*Mở mục xem chi tiết các dịch vụ*/
+        } else if (swap === false) {
+            $('.another-close-button img').attr("src", "../images/Homepage/another-service-more-icon.png");          /*Thay đổi nút đóng -> xem thêm*/
+            $('.another-service .content .icon .another-other').slideUp("slow");                                     /*Mở mục xem chi tiết các dịch vụ*/
+        }
+        swap = !swap;
+    });
+});
+
+/*JS cho Nội dung chia sẻ*/
+$(function () {
+    $("#next-to-noidungchiase").click(function () {                                                                 /*Click nút xuống nội dung chia sẻ*/
+        $('html, body').animate({
+            scrollTop: $('#noidungchiase').offset().top
+        }, 500);
+    });
+});
+
+$('.colum-content').hover(
+        function () {
+            $(this).find(".colum-img").find("img").css("transform", "scale(1.0)");                          /*Hover ảnh bài viết*/
+            $(this).find(".hover").css('transform', 'rotate(-6deg)');                                       /*Hover nút xem chi tiết bài viết*/
+        },
+        function () {
+            $(this).find(".colum-img").find("img").css("transform", "scale(1.1)");                          /*Hover ảnh bài viết*/
+            $(this).find(".hover").css('transform', 'rotate(0deg)');                                        /*Hover nút xem chi tiết bài viết*/
+        }
+);
+
